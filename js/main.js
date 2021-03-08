5266a92ba5286f220c18bbbe913b64d113d76572
Eric Tseng <amperic05@cengguanyudeMacBook-Pro.local>
2021-03-08 09:35:58 -0800

    js update

--- js/main.js
+++ js/main.js
@@ -4,8 +4,8 @@
 
 //         res.json().then(data => {
 //             $(document).ready(() => {
-//                 for(const imgData of data){
-//                     $('figure').append(`<img src=${imgData.url}></img>`)
+//                 for(const obj of data){
+//                     $('figure').append(`<img src=${obj.url}></img>`)
 //                 }
 //             });
 //         });
@@ -25,52 +25,24 @@ function filterID() {
         res.json()
         .then(data => {
             $(document).ready(()=>{
+
                 $('figure').empty();
+
                 const selectValue = document.forms["selectPhoto_form"]["photoID"].value;
+                let imgNum = 0;
                 let startIndex = 0;
-                let endIndex = 0;
 
-                // $('figure').append(`<img src=${data.filter((value, index) => {
-                    
-                
-                //     if(index==0){
-                //         return value;
-                //     }
-                // }).url}></img>`)
+                if(selectValue == 'all'){
+                    startIndex = 0;
+                    imgNum = 500;
+                }else{
+                    startIndex = parseInt(selectValue);
+                    imgNum = 100;
+                }
 
-                
-                switch(selectValue){
-                    case 'all':
-                        startIndex = 0;
-                        endIndex = data.length;
-                        break;
-                    case '0':
-                        startIndex = 0;
-                        endIndex = 100;
-                        break;
-                    case '100':
-                        startIndex = 100;
-                        endIndex = 200;
-                        break;
-                    case '200':
-                        startIndex = 200;
-                        endIndex = 300;
-                        break;
-                    case '300':
-                        startIndex = 300;
-                        endIndex = 400;
-                        break;
-                    case '400':
-                        startIndex = 400;
-                        endIndex = 500;
-                        break;
-                };
-
-                for(i=startIndex; i<endIndex; i++){
+                for(i=startIndex; i<startIndex+imgNum; i++){
                     $('figure').append(`<div><img src=${data[i].url}></img><h3>Title: ${data[i].title}</h3><h4>Album ID: ${data[i].albumId}</h4><h4>ID: ${data[i].id}</h4></div>`)
                 }
-                
-                
             })
         });
     })
